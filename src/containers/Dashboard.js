cb2314e00a0ccb3a5c5c5eca495e7b84cbb1d18b
pelagia.sykoudi@philips.com <pelagia.sykoudi@philips.com>
2018-04-15 18:01:40 +0200

    set up grid

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,7 +1,30 @@
+
 // @flow
 import React from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
+import grid from 'easy-grid';
+import './Dashboard.css';
+
+const PlaidGrid = styled(grid`
+    1fr   1fr   1fr   1fr   1fr
+1fr A,H   A,I   A,J   A,K   A,L
+1fr B,H   B,I   A,J   B,K   B,L
+1fr C,H   C,I   C,J   C,K   C,L 
+1fr D,H   D,I   D,J   D,K   D,L
+1fr E,H   E,I   E,J   E,K   A,L
+`)`
+height: 1000px;
+width: 1000px;
+`;
+
+const TwoByTwoLayout = grid`
+    1fr   1fr
+1fr A     A,B
+1fr A     A,B
+`;
+
+
 
 export class Dashboard extends React.PureComponent {
   state = {
@@ -10,7 +33,234 @@ export class Dashboard extends React.PureComponent {
 
   render() {
     return (
-      <div>Hello</div>
+      <div class="wrapper">
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TW</div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">Start</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box">TW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DW</div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">DL</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TW</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box">TL</div>
+
+      </div>
     );
   }
 }
